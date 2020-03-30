ce56d8eff2be63fb444daea336960b7cf1d7cc96
asifulmamun <asifulmamun@gmail.com>
2020-03-30 10:12:25 +0600

    connecting check

--- configuration/conn.php
+++ configuration/conn.php
@@ -1,16 +1,22 @@
 <?php
-    // // Local Server
-    // $servername = "localhost";
-    // $username = "root";
-    // $password = "";
-    // $dbname = "febbms";
+    // web link
+    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 
-    // Live Server    
-    $servername = "localhost";
-    $username = "easylabp_febbms";
-    $password = "#Febbms2020";
-    $dbname = "easylabp_febbms";
-
+    if($actual_link == 'http://blood.easylabpic.com'){
+        // Live Server    
+        $servername = "localhost";
+        $username = "easylabp_febbms";
+        $password = "#Febbms2020";
+        $dbname = "easylabp_febbms";
+    }else{
+        // Local Server
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "febbms";
+    }
+    
+    
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -19,8 +25,6 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 
-    echo $actual_link;
 
 ?>
